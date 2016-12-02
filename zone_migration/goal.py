79878573bcba88d95b0d67bd55891774e7c80129
Hidekazu Nakamura <hid-nakamura@vf.jp.nec.com>
2016-12-02 12:51:59 +0900

    Add zone migration goal

---
+++ zone_migration/goal.py
@@ -0,0 +1,22 @@
+from watcher._i18n import _
+from watcher.decision_engine.goal import base
+from watcher.decision_engine.goal.efficacy import specs
+
+class ZoneMigration(base.Goal):
+
+    @classmethod
+    def get_name(cls):
+        return "zone_migration"
+
+    @classmethod
+    def get_display_name(cls):
+        return _("Zone Migration")
+
+    @classmethod
+    def get_translatable_display_name(cls):
+        return "Zone Migration"
+
+    @classmethod
+    def get_efficacy_specification(cls):
+        return specs.Unclassified()
+
