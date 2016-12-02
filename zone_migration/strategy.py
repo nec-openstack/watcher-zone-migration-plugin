4452f338146c000e9f493d62dfe18af5021ae2f7
Hidekazu Nakamura <hid-nakamura@vf.jp.nec.com>
2016-12-02 14:38:35 +0900

    Implement zone migration strategy for confirming this plugin work
    This works with 'audit create' to execute nop actions.

--- zone_migration/strategy.py
+++ zone_migration/strategy.py
@@ -2,11 +2,18 @@ import abc
 
 import six
 
+from oslo_log import log
+
 from watcher._i18n import _
 from watcher.decision_engine.strategy.strategies import base
 
+LOG = log.getLogger(__name__)
+
+
 @six.add_metaclass(abc.ABCMeta)
 class ZoneMigrationStrategy(base.BaseStrategy):
+
+    NOP = "nop"
 
     def __init__(self, config, osc=None):
         super(ZoneMigrationStrategy, self).__init__(config, osc)
@@ -15,7 +22,15 @@ class ZoneMigrationStrategy(base.BaseStrategy):
         pass
 
     def do_execute(self):
-        pass
+        para1 = self.input_parameters.para1
+        para2 = self.input_parameters.para2
+        LOG.debug("para1:" + para1)
+        LOG.debug("para2:" + para2)
+
+        self.solution.add_action(action_type=self.NOP,
+                                 input_parameters={'message': para1})
+        self.solution.add_action(action_type=self.NOP,
+                                 input_parameters={'message': para2})
 
     def post_execute(self):
         pass
@@ -35,6 +50,27 @@ class ZoneMigrationStrategy(base.BaseStrategy):
     @classmethod
     def get_translatable_display_name(cls):
         return "Zone migration strategy"
+
+    @classmethod
+    def get_schema(cls):
+        return {
+            "properties": {
+                "para1": {
+                    "description": "string parameter one",
+                    "type": "string",
+                    "default": "hello zone one"
+                },
+                "para2": {
+                    "description": "string parameter two",
+                    "type": "string",
+                    "default": "hello zone two"
+                }
+            }
+        }
+     
+       
+
+
 
     
 
