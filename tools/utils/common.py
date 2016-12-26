23c42734080146376a2e6a135406646844c59bad
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-12-26 15:52:10 +0900

    Align pep8 rules

--- tools/utils/common.py
+++ tools/utils/common.py
@@ -1,20 +1,34 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 import os
-import time
 import sys
+import time
 import yaml
 
-from keystoneauth1 import loading
-from keystoneauth1 import session
-from keystoneauth1.exceptions import http as ks_exceptions
-from keystoneclient.v3 import client as keystoneclient
 from glanceclient import client as glanceclient
 from glanceclient import exc as glance_exections
+from keystoneauth1.exceptions import http as ks_exceptions
+from keystoneauth1 import loading
+from keystoneauth1 import session
+from keystoneclient.v3 import client as keystoneclient
 from novaclient import client as novaclient
 from novaclient import exceptions as nova_exections
+
 
 def load_target_env(path):
     with open(path, 'r') as io:
         return yaml.load(io)
+
 
 def env(*args, **kwargs):
     """Returns the first environment variable set.
@@ -27,20 +41,25 @@ def env(*args, **kwargs):
             return value
     return kwargs.get('default', '')
 
+
 def admin_session(**kwargs):
     loader = loading.get_plugin_loader('v3password')
     auth = loader.load_from_options(**kwargs)
     return session.Session(auth=auth)
 
+
 def keystone_client(session):
     keystone = keystoneclient.Client(session=session)
     return keystone
 
+
 def nova_client(session):
     return novaclient.Client('2.1', session=session)
 
+
 def glance_client(session):
     return glanceclient.Client('1', session=session)
+
 
 def get_role(keystone, name_or_id):
     try:
@@ -54,6 +73,7 @@ def get_role(keystone, name_or_id):
             raise ValueError('Role name seems ambiguous: %s' % name_or_id)
         return roles[0]
 
+
 def get_user(keystone, name_or_id):
     try:
         user = keystone.users.get(name_or_id)
@@ -65,6 +85,7 @@ def get_user(keystone, name_or_id):
         if len(users) > 1:
             raise ValueError('User name seems ambiguous: %s' % name_or_id)
         return users[0]
+
 
 def get_project(keystone, name_or_id):
     try:
@@ -78,6 +99,7 @@ def get_project(keystone, name_or_id):
             raise ValueError('Project name seems ambiguous: %s' % name_or_id)
         return projects[0]
 
+
 def get_domain(keystone, name_or_id):
     try:
         domain = keystone.domains.get(name_or_id)
@@ -90,12 +112,14 @@ def get_domain(keystone, name_or_id):
             raise ValueError('Domain name seems ambiguous: %s' % name_or_id)
         return domains[0]
 
+
 def get_flavor(nova, name_or_id):
     try:
         flavor = nova.flavors.get(name_or_id)
         return flavor
     except nova_exections.NotFound:
         return nova.flavors.find(name=name_or_id)
+
 
 def get_image(glance, name_or_id):
     try:
@@ -113,14 +137,16 @@ def get_image(glance, name_or_id):
             raise ValueError('Image name seems ambiguous: %s' % name_or_id)
         return _images[0]
 
+
 def create_session(user):
     loader = loading.get_plugin_loader('v3password')
     auth = loader.load_from_options(
-        auth_url = env('OS_AUTH_URL', default=None),
-        password = user['password'],
-        user_id = user['user'].id,
-        project_id = user['user'].default_project_id)
+        auth_url=env('OS_AUTH_URL', default=None),
+        password=user['password'],
+        user_id=user['user'].id,
+        project_id=user['user'].default_project_id)
     return session.Session(auth=auth)
+
 
 def create_user(keystone, user):
     project = get_project(keystone, user['project'])
@@ -136,12 +162,14 @@ def create_user(keystone, user):
         keystone.roles.grant(role.id, user=_user.id, project=project.id)
     return _user
 
+
 def delete_user(keystone, user):
     try:
         user = get_user(keystone, user['username'])
         keystone.users.delete(user)
-    except ValueError as e:
+    except ValueError:
         pass
+
 
 def create_users(keystone, users={}):
     _users = {}
@@ -151,9 +179,11 @@ def create_users(keystone, users={}):
         _users[key]['session'] = create_session(user)
     return _users
 
+
 def delete_users(keystone, users={}):
     for _, user in users.items():
         delete_user(keystone, user)
+
 
 def wait_instance(nova, instance, timeout=300):
     _timeout = 0
@@ -165,8 +195,8 @@ def wait_instance(nova, instance, timeout=300):
             raise RuntimeError("Timeout!")
         instance = nova.servers.get(instance.id)
 
+
 def create_server(env, name, vm, users, timeout=300):
-    nova_admin = nova_client(env['admin'])
     nova = nova_client(users[vm['user']]['session'])
     glance = glance_client(users[vm['user']]['session'])
     flavor = get_flavor(nova, vm['flavor'])
@@ -186,6 +216,7 @@ def create_server(env, name, vm, users, timeout=300):
     if vm['status'] == 'shutoff':
         wait_instance(nova, instance, timeout)
         instance.stop()
+
 
 def create_servers(env, vms, users):
     for name, vm in vms.items():
